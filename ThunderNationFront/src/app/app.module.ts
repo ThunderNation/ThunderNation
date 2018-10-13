435e420b35fb9c6b52e5e034458ed58cdbd4b052
wariorx <rcalcamo@uncc.edu>
2018-10-13 17:20:30 -0400

    Set up basic angular structure

--- ThunderNationFront/src/app/app.module.ts
+++ ThunderNationFront/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './support/sidenav/sidenav.component';
+import { LineChartComponent } from './support/line-chart/line-chart.component';
+import { BarChartComponent } from './support/bar-chart/bar-chart.component';
+import { PieChartComponent } from './support/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    LineChartComponent,
+    BarChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule
