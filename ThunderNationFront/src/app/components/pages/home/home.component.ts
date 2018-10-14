e501c2a03de049bbf4e05e8d07e04aea7ac0e2db
jelloh <abenedi3@uncc.edu>
2018-10-14 08:49:37 -0400

    add sliders again

--- ThunderNationFront/src/app/components/pages/home/home.component.ts
+++ ThunderNationFront/src/app/components/pages/home/home.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  imageSources = ['../../../../assets/slider-images/Zapdos.png',
-                  '../../../../assets/slider-images/img1.jpg',
+  imageSources = ['../../../../assets/slider-images/img1.jpg',
                   '../../../../assets/slider-images/img2.jpg']
 
   constructor() { }
