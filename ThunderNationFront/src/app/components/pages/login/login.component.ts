31d2776f2ba85d919877e9b1f02ed2014acf69a1
Devansh Desai <ddesai14@uncc.edu>
2018-10-14 01:43:54 -0400

    stuff

--- ThunderNationFront/src/app/components/pages/login/login.component.ts
+++ ThunderNationFront/src/app/components/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { ChartDataService }  from './../../../services/chart-data.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  username = "";
+  password = "";
+  title = 'UAT Account Selector Tool';
+  errorMessage = 'The username or password is incorrect. Please try again.'
+  invalidEntry = false;
+  constructor(private _backendService: ChartDataService, private _router: Router) { }
+  
   ngOnInit() {
+    history.pushState(null, null, location.href);
+    window.onpopstate = function(event) {
+     history.go(1);
+    };
   }
 
+  button_click() {
+      
+  }
+  login(){
+
+    this.invalidEntry = false;
+    if(this.username.length < 3 || this.password.length < 3){
+      this.invalidEntry = true;
+      this.password = ""
+    }else{
+      
+      this._backendService.login(this.username, this.password).subscribe(
+        response => {
+
+          alert("Login Succesful");
+          // localStorage.setItem("namID", this.username);
+          // localStorage.setItem("token", response['access_token'])
+          // this._backendService.checkUser(this.username).subscribe(
+          //   response =>{
+              
+          //     if(response['items'] == true){
+          //       this._router.navigate(['/home']);
+          //     }else{
+          //       this.invalidEntry = true;
+          //       this.errorMessage = "You do not have access to this application. Contact support."
+          //     }
+          //   },
+          //   error => {
+          //     this.errorMessage = "AD systems is currently offline. Please try again later."
+          //   }
+          // )
+          
+        },
+        error => {
+          this.invalidEntry = true;
+          this.errorMessage = "The username or password is incorrect. Please try again."
+        }
+      );
+    }
+    
+  }
 }
--- ThunderNationFront/src/app/services/chart-data.service.ts
+++ ThunderNationFront/src/app/services/chart-data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import {Observable, of} from 'rxjs';
 import {catchError, debounceTime, distinctUntilChanged, map, tap, switchMap} from 'rxjs/operators'
+import { headersToString } from 'selenium-webdriver/http';
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +18,20 @@ export class ChartDataService {
 
     return this.http.get<any>(url, {params: parameters} );
   };
+
+  login(username, password): Observable<any[]> {
+    let header = new Headers();
+    header.append('username', username)
+    header.append('password', password)
+
+    return this.http.post('https://emp-dev-api.duke-energy.com/', {
+      headers: header
+    })
+    .map(response => response.json())
+    .catch(error => Observable.throw(error));
+  }
+
+  
 
   // getData2(url:string, params:any) {
   //   if(url === '' )
