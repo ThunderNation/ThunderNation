88d33b347f210517e9224cb445832a8fbf4a7902
jelloh <abenedi3@uncc.edu>
2018-10-13 21:19:19 -0400

    set up theme

---
+++ ThunderNationFront/src/app/material.module.ts
@@ -0,0 +1,37 @@
+
+import { NgModule } from '@angular/core';
+
+// Material Stuff
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+import {MatBadgeModule} from '@angular/material/badge';
+
+
+@NgModule({
+  imports: [
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatBadgeModule,
+],
+  exports: [
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatBadgeModule,
+  ],
+})
+export class MaterialModule { }
