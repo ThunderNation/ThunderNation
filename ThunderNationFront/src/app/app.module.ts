9613fcf35bc079be1865bb6914ed57b4c5d3a692
jelloh <abenedi3@uncc.edu>
2018-10-13 20:05:49 -0400

    side nav

--- ThunderNationFront/src/app/app.module.ts
+++ ThunderNationFront/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+
 
 import { AppComponent } from './app.component';
-import { SidenavComponent } from './support/sidenav/sidenav.component';
-import { LineChartComponent } from './support/line-chart/line-chart.component';
-import { BarChartComponent } from './support/bar-chart/bar-chart.component';
-import { PieChartComponent } from './support/pie-chart/pie-chart.component';
+import { HomeComponent } from './components/pages/home/home.component';
+
+import { SidenavComponent } from './components/support/sidenav/sidenav.component';
+import { LineChartComponent } from './components/support/line-chart/line-chart.component';
+import { BarChartComponent } from './components/support/bar-chart/bar-chart.component';
+import { PieChartComponent } from './components/support/pie-chart/pie-chart.component';
 
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
 
   // { path: 'crisis-center', component: CrisisListComponent },
   // { path: 'hero/:id',      component: HeroDetailComponent },
@@ -28,6 +38,7 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     SidenavComponent,
     LineChartComponent,
     BarChartComponent,
@@ -37,13 +48,17 @@ const appRoutes: Routes = [
     BrowserModule,
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
-    // other imports here
+      // { enableTracing: true } // <-- debugging purposes only
+    ),
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-//HI
